3a785ed393235eafa174ec137b96d967563b2343
Shane Smiskol <shane@smiskol.com>
2023-10-18 16:53:26 -0700

    block LTA_RELATED

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -303,7 +303,7 @@ static int toyota_fwd_hook(int bus_num, int addr) {
   if (bus_num == 2) {
     // block stock lkas messages and stock acc messages (if OP is doing ACC)
     // in TSS2, 0x191 is LTA which we need to block to avoid controls collision
-    int is_lkas_msg = ((addr == 0x2E4) || (addr == 0x412) || (addr == 0x191));
+    int is_lkas_msg = ((addr == 0x2E4) || (addr == 0x412) || (addr == 0x191) || (addr == 0x371));
     // in TSS2 the camera does ACC as well, so filter 0x343
     int is_acc_msg = (addr == 0x343);
     int block_msg = is_lkas_msg || (is_acc_msg && !toyota_stock_longitudinal);
